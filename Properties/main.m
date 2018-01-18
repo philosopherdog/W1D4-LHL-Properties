a5de44f705b9e6afdc51a78c511e9cf6da46f8b8
steve thompson <steven.thompson.ca@gmail.com>
2018-01-18 09:49:29 -0500

    Initial Commit

---
+++ Properties/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Properties
+//
+//  Created by steve on 2018-01-18.
+//  Copyright © 2018 steve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
